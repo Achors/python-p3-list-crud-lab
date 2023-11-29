5fab559e641826317eca26c83a16836faf1a03b7
Adolf <adolfnyerere@gmail.com>
2023-11-29 10:03:34 +0300

    few errors passed

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,38 @@
 def create_an_empty_list():
-    return None
+    return []
 
-def create_a_list():
-    return None
+def create_a_list(self):
+    return []
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l.insert(0, element)
+    print(l)
 
 def remove_element_from_end_of_list(l):
-    return None
+    if l:
+        l.pop()
 
 def remove_element_from_start_of_list(l):
-    return None
+    if l:
+        del l[0]
 
 def retrieve_first_element_from_list(l):
-    return None
+    if l:
+        return l[0]
+    else:
+        return None
 
 def retrieve_element_from_index(l, index):
-    return None
+    if 0 <= index < len(l):
+        return l[l]
+    else:
+        return None
 
 def retrieve_last_element_from_list(l):
-    return None
+    if l:
+        return l[-1]
+    else:
+        return None
